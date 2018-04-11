8e9b0e575fd28740adefe78dd72d69a0a5e56ad3
Timtro <timtro@gmail.com>
2018-04-11 11:47:51 -0400

    Added recurrant mean and variance as fold. Preludes 2-3.

--- test/utest-linear_least_squares.cpp
+++ test/utest-linear_least_squares.cpp
@@ -1,7 +1,75 @@
 #include "utest-linear_least_squares.hpp"
+#include <algorithm>
 #include <catch/catch.hpp>
+#include <iostream>
+#include <numeric>
+#include <tuple>
+#include <vector>
 
-// The oracle's polynomial from Beckman (2017) Kalman Folding Pt. 1
+/* Preludes:
+ * In [Beckman2016], Beckman introduces the static Kalman filter in a series of
+ * four preludes, the fourth being an implementation of an actual Kalman filter.
+ * This series of unit tests employ those recurrant relations and folding to
+ * reproduce the results from his Wolfram code.
+ */
+
+auto bulk_average = [](auto xs) {
+  return std::accumulate(cbegin(xs), cend(xs), 0.f) / xs.size();
+};
+
+// Prelude 2
+TEST_CASE("Functional folding for mean on…", "[prelude2]") {
+  // NOT A REAL UNIT TEST: No external code dependencies.
+
+  using State = std::pair<double, unsigned>;
+  auto cume = [](State xANDn, double z) -> State {
+    // with…
+    const auto [x, n] = xANDn;
+    const double K = 1.f / (n + 1);
+    
+    return {x + K * (z - x), n + 1};
+  };
+
+  SECTION("… a vector") {
+    REQUIRE(cume({0, 0}, 1.414).second == 1);
+    REQUIRE(cume({0, 0}, 1.414).first == Approx(1.414));
+
+    auto data = std::vector<double>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const auto [average, count] =
+        std::accumulate(cbegin(data), cend(data), State{0, 0}, cume);
+    REQUIRE(count == data.size());
+    REQUIRE(average == Approx(bulk_average(data)));
+  }
+}
+
+// Prelude 3
+TEST_CASE("Functional folding for mean and variance on…", "[prelude3]") {
+  // NOT A REAL UNIT TEST: No external code dependencies.
+  using State = std::tuple<double, double, unsigned int>;
+  auto cume = [](State varANDxANDn, double z) -> State {
+    // with…
+    const auto [var, x, n] = varANDxANDn;
+    const auto KforMean = 1. / (n + 1);
+    const auto KforVariance = n * KforMean;
+    const auto residual = z - x;
+    const auto x2 = x + KforMean * residual;
+    const auto ssr2 = (n - 1) * var + KforVariance * residual * residual;
+
+    return {ssr2 / std::max<unsigned>(n, 1), x2, n + 1};
+  };
+
+  SECTION("… a vector") {
+
+    auto data = std::vector<double>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const auto [variance, average, count] =
+        std::accumulate(cbegin(data), cend(data), State{0, 0, 0}, cume);
+    REQUIRE(count == data.size());
+    REQUIRE(variance == Approx(9.1667));
+    REQUIRE(average == Approx(bulk_average(data)));
+  }
+}
+
+// The oracle's polynomial.
 double f(double x) { return -5 * x * x * x - 4 * x * x + 9 * x - 3; }
 
 TEST_CASE("Test polynomial") {
